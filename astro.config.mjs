ce1e7ac3a9d68da517971ac004c0f5f9284cb0a3
Andres Ijurra <andres.ijurra@usach.cl>
2025-02-04 01:34:03 -0300

    fix: Set output to 'hybrid' in astro.config.mjs for improved build configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
   integrations: [tailwind()],
   adapter: vercel({
     imageService: true,
