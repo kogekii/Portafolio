97f6a5ac684a6617ca9c4bef0b029b6724cbead6
Andres Ijurra <andres.ijurra@usach.cl>
2025-02-04 01:27:41 -0300

    fix: Update dependencies and configure Vercel adapter with image service support in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-
+import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapters: vercel({imageService: true}),
+  adapter: vercel({
+    imageService: true,
+    devImageService: 'sharp',
+  }),
 });
